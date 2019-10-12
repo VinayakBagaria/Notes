d404e60b84e58db9dc4843e174aeb0fd1e3ec683
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 17:47:36 +0530

    edit note success

--- ui/src/EditNote.js
+++ ui/src/EditNote.js
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { notify } from 'react-notify-toast';
 
-function EditNote() {
+const NOTE_QUERY = gql`
+  query getNote($_id: ID!) {
+    getNote(_id: $_id) {
+      _id
+      title
+      content
+      date
+    }
+  }
+`;
+
+const UPDATE_NOTE = gql`
+  mutation updateNote($_id: ID!, $title: String, $content: String) {
+    updateNote(_id: $_id, input: { title: $title, content: $content }) {
+      _id
+      title
+      content
+    }
+  }
+`;
+
+function EditNote({ match }) {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const { loading, error, data: note } = useQuery(NOTE_QUERY, {
+    variables: {
+      _id: match.params.id,
+    },
+  });
+
+  const [updateNote] = useMutation(UPDATE_NOTE);
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    updateNote({
+      variables: {
+        _id: note.getNote._id,
+        title: title ? title : note.getNote.title,
+        content: content ? content : note.getNote.content,
+      },
+    });
+    notify.show('Note was edited successfully');
+  }
+
+  if (loading) {
+    return 'Fetching note...';
+  }
+  if (error) {
+    return 'Error fetching note';
+  }
+
   return (
     <div className="container m-t-20">
       <h1 className="page-title">Edit Note</h1>
       <div className="newnote-page m-t-20">
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="field">
             <label className="label">Note Title</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Note Title" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Note Title"
+                defaultValue={note.getNote.title}
+                onChange={e => setTitle(e.target.value)}
+              />
             </div>
           </div>
           <div className="field">
@@ -19,6 +79,8 @@ function EditNote() {
                 className="textarea"
                 rows="10"
                 placeholder="Note Content here..."
+                defaultValue={note.getNote.content}
+                onChange={e => setContent(e.target.value)}
               />
             </div>
           </div>
