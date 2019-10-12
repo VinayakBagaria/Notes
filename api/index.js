f9baaac277cdff35971fe61bcc31f8295d122ec3
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 05:58:50 +0530

    configuring mongoose

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/notetaking_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 const app = express();
 const PORT = 4300;
