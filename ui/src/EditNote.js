6955778b19806e889e9b3512eefdd17a29dff123
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 16:15:35 +0530

    add and edit note dummy comps

---
+++ ui/src/EditNote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EditNote() {
+  return <h1>Hey</h1>;
+}
+
+export default EditNote;
---
+++ ui/src/NewNote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewNote() {
+  return <h1>Hey</h1>;
+}
+
+export default NewNote;
