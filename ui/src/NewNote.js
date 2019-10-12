070ca86c7238d0f175c5a35bb52ae058f7f2bdb5
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 17:30:48 +0530

    create note mutation

--- ui/src/NewNote.js
+++ ui/src/NewNote.js
@@ -1,15 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-function NewNote() {
+const NEW_NOTE = gql`
+  mutation createNoteMutation($title: String!, $content: String!) {
+    createNote(input: { title: $title, content: $content }) {
+      _id
+      title
+      content
+      date
+    }
+  }
+`;
+
+const NOTES_QUERY = gql`
+  {
+    allNotes {
+      _id
+      title
+      content
+      date
+    }
+  }
+`;
+
+const NewNote = withRouter(({ history }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const [createNote] = useMutation(NEW_NOTE, {
+    update(cache, { data: createdNewNote }) {
+      const { allNotes } = cache.readQuery({ query: NOTES_QUERY });
+      cache.writeQuery({
+        query: NOTES_QUERY,
+        data: { allNotes: allNotes.concat([createdNewNote]) },
+      });
+    },
+  });
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    createNote({
+      variables: {
+        title,
+        content,
+        date: Date.now(),
+      },
+    });
+    history.push('/');
+  }
+
   return (
     <div className="container m-t-20">
       <h1 className="page-title">New Note</h1>
       <div className="newnote-page m-t-20">
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="field">
             <label className="label">Note Title</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Note Title" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Note Title"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+              />
             </div>
           </div>
           <div className="field">
@@ -19,6 +75,8 @@ function NewNote() {
                 className="textarea"
                 rows="10"
                 placeholder="Note Content here..."
+                value={content}
+                onChange={e => setContent(e.target.value)}
               />
             </div>
           </div>
@@ -31,6 +89,6 @@ function NewNote() {
       </div>
     </div>
   );
-}
+});
 
 export default NewNote;
