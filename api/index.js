7c25d1b054a84e04be99e679b766a8d96d158487
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 17:09:21 +0530

    cors package added

--- api/index.js
+++ api/index.js
@@ -1,23 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/notetaking_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 const PORT = 4300;
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Note api v1'
-    });
+  res.json({
+    message: 'Note api v1',
+  });
 });
 app.listen(PORT, () => {
-    console.log(`Running on http://localhost:${PORT}`);
+  console.log(`Running on http://localhost:${PORT}`);
 });
