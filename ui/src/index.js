212cbbd1265ecb0effdd3050146f30b5436b8cbe
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 17:15:37 +0530

    apollo errors and showing them in a toast

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,12 +5,19 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import Notifications, { notify } from 'react-notify-toast';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 const httpLink = createHttpLink({ uri: 'http://localhost:4300/graphql' });
-const link = ApolloLink.from([httpLink]);
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => notify.show(message, 'error'));
+  }
+});
+const link = ApolloLink.from([errorLink, httpLink]);
 
 const client = new ApolloClient({
   link,
@@ -19,6 +26,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
+    <Notifications />
     <App />
   </ApolloProvider>,
   document.getElementById('root')
