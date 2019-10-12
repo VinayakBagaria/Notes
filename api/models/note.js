be3c9d1ec151b86ed1edff774019bdc30f136eba
Vinayak Bagaria <workvinayak95@gmail.com>
2019-10-12 06:01:37 +0530

    note model

---
+++ api/models/note.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+export default mongoose.model('note', NoteSchema);
